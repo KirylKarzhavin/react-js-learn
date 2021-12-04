39afb862606b2953baab70a1dc57fe9f98e92338
Kiryl Karzhavin <you@example.com>
2021-12-04 20:03:53 +0100

    testing project on main computer

--- react-learn-01/src/App.js
+++ react-learn-01/src/App.js
@@ -1,16 +1,7 @@
 import './App.css';
 
 const App = () => {
-  return (
-    <div className='App'>
-      <ul>
-        <li>css</li>
-        <li>html</li>
-        <li>js</li>
-        <li>react</li>
-      </ul>
-    </div>
-  );
+  return <div className='App'></div>;
 };
 
 export default App;
